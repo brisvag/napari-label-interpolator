a7998633e1c3fbe5752ea9dfecf4be644b1cd1dc
Lorenzo Gaifas <brisvag@gmail.com>
2022-09-08 17:08:20 +0200

    fix gitignore, add test

--- src/napari_label_interpolator/_tests/test_widget.py
+++ src/napari_label_interpolator/_tests/test_widget.py
@@ -5,5 +5,10 @@ from napari_label_interpolator.interpolator import interpolate
 
 def test_interpolator(make_napari_viewer):
     viewer = make_napari_viewer()
-    viewer.add_labels(np.random.random((100, 100)))
-    viewer.window.add_widget(interpolate)
+    labels = np.ones((3, 3), dtype=int)
+    labels[1] = 0
+    viewer.add_labels(labels)
+    w = interpolate()
+    viewer.window.add_dock_widget(w)
+    w()
+    assert np.all(viewer.layers[-1].data == 1)
